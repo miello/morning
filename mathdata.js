30aa7ea4a76c583c0b4b6801f3fb7f6fcfe2806f
miello <35169079+miello@users.noreply.github.com>
2018-01-09 07:34:01 +0700

    Update mathdata.js

--- mathdata.js
+++ mathdata.js
@@ -1,6 +1,6 @@
 //โจทย์เลขลงนี้
 //Latex หรือ Picture Only
-q[0][0] = "ให้ x,y เป็นจำนวนจริงที่ทำให้ \(5x^2+2y^2+2xy-6x-6y+5=0\) และ \(3x+3y-5=0\) <br> จงหาค่าของ \(y \over x\)";
+q[0][0] = "ให้ x,y เป็นจำนวนจริงที่ทำให้ <img = src="http://latex.codecogs.com/gif.latex?5x^2+2y^2+2xy-6x-6y+1" border ="0"/> และ <img = src="http://latex.codecogs.com/gif.latex?3x+3y-5=0" border ="0"/> <br> จงหาค่าของ <img = src="http://latex.codecogs.com/gif.latex?y/x" border ="0"/>";
 choi[0][0][0] = "1";
 choi[0][0][1] = "2";
 choi[0][0][2] = "3";
