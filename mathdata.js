e1eb723a9f6e0cec56458ea4d7b342f91086d011
naniyori <31788401+naniyori@users.noreply.github.com>
2018-02-03 15:16:29 +0700

    Update mathdata.js

--- mathdata.js
+++ mathdata.js
@@ -1,5 +1,6 @@
-qpic[0][0]="M1.png";
-qpic[0][1]="M2.png";
-qpic[0][2]="M3.png";
-qpic[0][3]="M4.png";
+qpic[0][0]="image/M1.png";
+qpic[0][1]="image/M2.png";
+qpic[0][2]="image/M3.png";
+qpic[0][3]="image/M4.png";
+
 
