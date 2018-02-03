b0dc006c7cdfd41dd9e27aa5a872952b4b43344b
naniyori <31788401+naniyori@users.noreply.github.com>
2018-02-03 15:09:43 +0700

    Update mathdata.js

--- mathdata.js
+++ mathdata.js
@@ -1,8 +1,5 @@
-//โจทย์เลขลงนี้
-//Latex หรือ Picture Only
-//q[0][0] = "ให้ x,y เป็นจำนวนจริงที่ทำให้ <img = src="http://latex.codecogs.com/gif.latex?5x^2+2y^2+2xy-6x-6y+1" border ="0"/> และ <img = src="http://latex.codecogs.com/gif.latex?3x+3y-5=0" border ="0"/> <br> จงหาค่าของ <img = src="http://latex.codecogs.com/gif.latex?y/x" border ="0"/>";
-choi[0][0][0] = "1";
-choi[0][0][1] = "2";
-choi[0][0][2] = "3";
-choi[0][0][3] = "4";
-cans[0][0] = 4;
+qpic[0][0]="M1.png";
+qpic[0][1]="M2.png";
+qpic[0][2]="M3.png";
+qpic[0][3]="M4.png";
+
